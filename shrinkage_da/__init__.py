f7619e6b2cba8a6d500e1b8c0fcd268ca29c75ac
Miika Ahdesmaki <miika.ahdesmaki@live.fi>
2019-01-20 10:54:06 +0000

    _sdaclass class defined using SDA functions

--- shrinkage_da/__init__.py
+++ shrinkage_da/__init__.py
@@ -1,8 +1,6 @@
-from ._template import TemplateEstimator
-from ._template import TemplateClassifier
-from ._template import TemplateTransformer
+from ._sdaclasse import ShrinkageDiscriminantAnalysis
 
 from ._version import __version__
 
-__all__ = ['TemplateEstimator', 'TemplateClassifier', 'TemplateTransformer',
+__all__ = ['ShrinkageDiscriminantAnalysis',
            '__version__']
